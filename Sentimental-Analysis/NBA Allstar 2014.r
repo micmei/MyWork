74fa09bb68708a8beb5f9b695175073577a052d3
Lanqiu Mei <micmei@utexas.edu>
2014-02-17 13:11:39 -0600

    Update NBA Allstar 2014.r

--- Sentimental-Analysis/NBA Allstar 2014.r
+++ Sentimental-Analysis/NBA Allstar 2014.r
@@ -9,9 +9,20 @@ require(stringr)
 require(scales)
 load("twitter authentication.Rdata")
 
+## As a side note, I have a deep understanding how heartbreaking it is to set up OAuth and get access to twitter API
+## But finally I think I found something that can help you out in 15 minutes
+## Here is the website.
+## https://sites.google.com/site/dataminingatuoc/home/data-from-twitter/r-oauth-for-twitter
+## You're welcome.
+
+## Ok time to get data
+## The precise geocode of BMO Harris Bradley Center is (43.042813, -87.916185), I googled that.
+
 filterStream(file = "20140216_nbaallstar.json", locations = c(42.5,-88.5,43.5,-87.5), 
              track=c("NBA","AllStar","nba","allstar"),timeout = 14400, oauth = cred)
 dataPath <- "20140216_nbaallstar.json"
+
+## Idat
 temp <- data.table(parseTweets(dataPath, simplify = FALSE, verbose = TRUE))
 tweets   <- temp[,c(2:8,10:42) := NULL]
 
@@ -40,16 +51,19 @@ tweets[,text := gsub("\t{2,}"," ",text)]
 tweets[,text := tolower(text)]
 
 ## calculate how much time has elapsed from the start
+## I only reported the 5-minute time steps in my blog since the trends themselves are almost the same
 tweets[,timestamp_second := as.numeric(time-time[[1]])]
 tweets[,timestamp_minute := floor(timestamp_second/60)]
 tweets[,timestamp_5minute := floor(timestamp_minute/5)]
 tweets[,timestamp_10minute := floor(timestamp_minute/10)]
 tweets[,timestamp_20minute := floor(timestamp_minute/20)]
-## 
+
+## Refer to http://www.cs.uic.edu/~liub/FBS/sentiment-analysis.html for more. 
 pol <- polarity(tweets$text)$all
 tweets[, words := pol$wc]
 tweets[, pol := pol$polarity]
 
+## Regress with smoothing
 lw1 <- with(tweets,loess(pol ~ timestamp_second))
 lw_1m <- with(tweets,loess(pol ~ timestamp_minute))
 lw_5m <- with(tweets,loess(pol ~ timestamp_5minute))
@@ -58,6 +72,7 @@ lw_5m_span25 <- with(tweets,loess(pol ~ timestamp_5minute, span=0.25))
 lw_10m <- with(tweets,loess(pol ~ timestamp_10minute))
 lw_20m <- with(tweets,loess(pol ~ timestamp_20minute))
 
+## Plot
 with(tweets,plot(timestamp_5minute,pol,ylim=c(-0.05,0.05),main="Sentimental Analysis of NBA AllStar 2014",
                  ylab="Sentiment (tweet-level polarity)",col="lightgrey"))
 mtext("Shooting Stars, Skills Challenge, Three-Point Contest, Slam Dunk")
